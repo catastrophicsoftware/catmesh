4c9e6e63911a83af2f0d3e328a529f491e349c31
cssoftware <contactcssoftware@gmail.com>
2020-05-02 13:27:30 -0700

    no message

--- CSSVault.py
+++ CSSVault.py
@@ -1,13 +1,12 @@
 import hvac
 import os
-import cssutility
 import time
 
 vaultClient = hvac.Client(url=os.environ['CATMESH_VAULT_ADDR'],token=os.environ['CATMESH_VAULT_TOKEN'])
 
 
 def requestAWSCredentialsV2(awsRoleName):
-    cssutility.log("Requesting AWS Credentials: " + awsRoleName)
+    print("Requesting AWS Credentials: " + awsRoleName)
 
     try:
         credResp = vaultClient.secrets.aws.generate_credentials(name=awsRoleName)
@@ -19,5 +18,5 @@ def requestAWSCredentialsV2(awsRoleName):
             credResp['data']['secret_key']
         )
     except Exception as ex:
-        cssutility.log("Exception Requesting AWS Credentials")
-        cssutility.log("Exception Details:" + str(ex))
+        print("Exception Requesting AWS Credentials")
+        print("Exception Details:" + str(ex))
